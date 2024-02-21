3f4fdbe2180860f6547dc6c15a3f201b8f459a46
Aryan <52215513+importly@users.noreply.github.com>
2024-02-21 12:28:17 -0500

    implemented mpu library

--- src/main.cpp
+++ src/main.cpp
@@ -3,39 +3,20 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP3XX.h>
 #include <rocket_log.h>
+#include <MPU6050.h>
+#include <KalmanFilter.h>
 #include <SD.h>
 #include <SPI.h>
 
 #define DEBUG 1
 #define SEA_LEVEL_PRESSURE_HPA (1013.25)
 
-#define ACCEL_SCALE_MODIFIER_2G 16384.0
-#define ACCEL_SCALE_MODIFIER_4G 8192.0
-#define ACCEL_SCALE_MODIFIER_8G 4096.0
-#define ACCEL_SCALE_MODIFIER_16G 2048.0
-
-#define GYRO_SCALE_MODIFIER_250DEG 131.0
-#define GYRO_SCALE_MODIFIER_500DEG 65.5
-#define GYRO_SCALE_MODIFIER_1000DEG 32.8
-#define GYRO_SCALE_MODIFIER_2000DEG 16.4
-
-#define ACCEL_RANGE_2G 0x00
-#define ACCEL_RANGE_4G 0x08
-#define ACCEL_RANGE_8G 0x10
-#define ACCEL_RANGE_16G 0x18
-
-#define GYRO_RANGE_250DEG 0x00
-#define GYRO_RANGE_500DEG 0x08
-#define GYRO_RANGE_1000DEG 0x10
-#define GYRO_RANGE_2000DEG 0x18
-
-
 // I2C addresses
-const uint8_t MPU6050_ADDRESS = 0x68;
 const uint8_t BMP390_ADDRESS = 0x77;
 
 // Devices
 Adafruit_BMP3XX bmp;
+MPU6050 mpu;
 
 Servo pulley;
 Servo steers[4];  // create an array of servo objects to control 4 servos
@@ -45,40 +26,27 @@ int calbration[4] = { 5, 0, -7, -6 };
 int BUZZER_PIN = 15;
 int pulleyint = 29;
 
+KalmanFilter kalmanX(0.001, 0.003, 0.03);
+KalmanFilter kalmanY(0.001, 0.003, 0.03);
+
+float accPitch = 0;
+float accRoll = 0;
+
+float kalPitch = 0;
+float kalRoll = 0;
+
 void setup() {
     Serial.begin(115200);
-
-    // MPU 6050
-    Wire.begin();
-
-    // Wake up MPU6050
-    Wire.beginTransmission(MPU6050_ADDRESS);
-    Wire.write(0x6B); // Power Management 1 register
-    Wire.write(0);    // Set to 0 to wake up the MPU6050
-    Wire.endTransmission(true);
-
-    // Configure Accelerometer sensitivity (±16g)
-    Wire.beginTransmission(MPU6050_ADDRESS);
-    Wire.write(0x1C);
-    Wire.write(ACCEL_RANGE_16G); // Accel full scale range ±16g
-    Wire.endTransmission(true);
-
-    // Configure Gyroscope sensitivity (±250 deg/s)
-    Wire.beginTransmission(MPU6050_ADDRESS);
-    Wire.write(0x1B);
-    Wire.write(GYRO_RANGE_250DEG); // Gyro full scale range ±250 deg/s
-    Wire.endTransmission(true);
-
-    // Set Low Pass Filter (Bandwidth 184Hz)
-    Wire.beginTransmission(MPU6050_ADDRESS);
-    Wire.write(0x1A);
-    Wire.write(0x01); // Low Pass Filter setting
-    Wire.endTransmission(true);
 
     // BMP 390
     if (bmp.begin_I2C(BMP390_ADDRESS, &Wire1))
         Serial.println("Found BMP390");
     else while (true) { Serial.println("Could not find a valid BMP390 sensor, check wiring!"); }
+
+    // MPU6050
+    if (mpu.begin(MPU6050_SCALE_250DPS, MPU6050_RANGE_16G))
+        Serial.println("Found MPU6050");
+    else while (true) { Serial.println("Could not find a valid MPU6050 sensor, check wiring!"); }
 
 
     // Set up oversampling and filter initialization
@@ -86,6 +54,7 @@ void setup() {
     bmp.setPressureOversampling(BMP3_OVERSAMPLING_32X);
     bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_1);
     bmp.setOutputDataRate(BMP3_ODR_200_HZ);
+
 
     pinMode(BUZZER_PIN, OUTPUT); // set the buzzer pin as output
 
@@ -97,6 +66,9 @@ void setup() {
     digitalWrite(BUZZER_PIN, HIGH); // turn the buzzer on again
     delay(200); // wait for 200ms
     digitalWrite(BUZZER_PIN, LOW); // turn the buzzer off
+
+
+    mpu.calibrateGyro();
 
     if (DEBUG) Serial.println("AccelRateX,AccelRateY,AccelRateZ,GyroRateX,GyroRateY,GyroRateZ,Altitude");
 }
@@ -110,11 +82,9 @@ int16_t readMPU6050(int address, uint8_t registerAddress) {
     return data;
 }
 
-int16_t accX, accY, accZ, gyroX, gyroY, gyroZ;
 float accelRateX, accelRateY, accelRateZ, gyroRateX, gyroRateY, gyroRateZ, altitude;
 
 bool calibrate = false;
-float accelRateX_c = 0, accelRateY_c = 0, accelRateZ_c=0, gyroRateX_c=0, gyroRateY_c=0, gyroRateZ_c= 0;
 float altitude_c = 0.0;
 
 int i = 0 ;
@@ -122,66 +92,62 @@ int i = 0 ;
 void loop() {
     analogWrite(pulleyint, 255);
 
-    // Read accelerometer data
-    accX = readMPU6050(MPU6050_ADDRESS, 0x3B);
-    accY = readMPU6050(MPU6050_ADDRESS, 0x3D);
-    accZ = readMPU6050(MPU6050_ADDRESS, 0x3F);
-
-    // Read gyroscope data
-    gyroX = readMPU6050(MPU6050_ADDRESS, 0x43);
-    gyroY = readMPU6050(MPU6050_ADDRESS, 0x45);
-    gyroZ = readMPU6050(MPU6050_ADDRESS, 0x47);
-
     // Read pressure data
     if (!bmp.performReading()) {
         Serial.println("Failed to perform reading :(");
         return;
     }
 
-    // Convert accelerometer data to g's
-    accelRateX = accX / ACCEL_SCALE_MODIFIER_16G;
-    accelRateY = accY / ACCEL_SCALE_MODIFIER_16G;
-    accelRateZ = accZ / ACCEL_SCALE_MODIFIER_16G;
+    Vector acc = mpu.readNormalizeAccel();
+    Vector gyr = mpu.readNormalizeGyro();
 
-    // Convert gyro data to degrees per second
-    gyroRateX = gyroX / GYRO_SCALE_MODIFIER_250DEG;
-    gyroRateY = gyroY / GYRO_SCALE_MODIFIER_250DEG;
-    gyroRateZ = gyroZ / GYRO_SCALE_MODIFIER_250DEG;
+    // Calculate Pitch & Roll from accelerometer (deg)
+    accPitch = -(atan2(acc.XAxis, sqrt(acc.YAxis*acc.YAxis + acc.ZAxis*acc.ZAxis))*180.0)/M_PI;
+    accRoll  = (atan2(acc.YAxis, acc.ZAxis)*180.0)/M_PI;
+
+    // Kalman filter
+    kalPitch = kalmanY.update(accPitch, gyr.YAxis);
+    kalRoll = kalmanX.update(accRoll, gyr.XAxis);
 
     // altitude in meters
     altitude = bmp.readAltitude(SEA_LEVEL_PRESSURE_HPA);
 
-    // apply all calibration values
-    accelRateX -= accelRateX_c;
-    accelRateY -= accelRateY_c;
-    accelRateZ -= accelRateZ_c;
-
-    gyroRateX -= gyroRateX_c;
-    gyroRateY -= gyroRateY_c;
-    gyroRateZ -= gyroRateZ_c;
-
     altitude -= altitude_c;
 
     if (!calibrate) {
-        accelRateX_c = accX;
-        accelRateY_c = accY;
-        accelRateZ_c = accZ;
-
-        gyroRateX_c = gyroX;
-        gyroRateY_c = gyroY;
-        gyroRateZ_c = gyroZ;
-
         altitude_c = altitude;
 
         calibrate = true;
     }
 
     if (DEBUG) {
-        Serial.print(accelRateX);
-        Serial.print(",");
-        Serial.print(accelRateY);
-        Serial.print(",");
-        Serial.println(accelRateZ);
+
+        Serial.print(accPitch);
+        Serial.print(":");
+        Serial.print(accRoll);
+        Serial.print(":");
+        Serial.print(kalPitch);
+        Serial.print(":");
+        Serial.print(kalRoll);
+        Serial.print(":");
+        Serial.print(acc.XAxis);
+        Serial.print(":");
+        Serial.print(acc.YAxis);
+        Serial.print(":");
+        Serial.print(acc.ZAxis);
+        Serial.print(":");
+        Serial.print(gyr.XAxis);
+        Serial.print(":");
+        Serial.print(gyr.YAxis);
+        Serial.print(":");
+        Serial.print(gyr.ZAxis);
+
+        Serial.println();
+//        Serial.print(accelRateX);
+//        Serial.print(",");
+//        Serial.print(accelRateY);
+//        Serial.print(",");
+//        Serial.println(accelRateZ);
 //        Serial.print(",");
 //        Serial.print(gyroRateX);
 //        Serial.print(",");
