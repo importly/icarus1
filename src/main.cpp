aa121a2074de5f52b5787f21b0d6cf4cd9e3408a
Aryan <52215513+importly@users.noreply.github.com>
2024-04-09 14:09:56 -0400

    First day of launches

--- src/main.cpp
+++ src/main.cpp
@@ -29,10 +29,11 @@ Servo sy2;
 // Beeper
 const int BUZZER_PIN = 36;
 const int PULLEY_PIN = 29;
-const int SX1_PIN = 3;
-const int SX2_PIN = 7;
-const int SY1_PIN = 10;
-const int SY2_PIN = 25;
+const int SY1_PIN = 3;
+const int SY2_PIN = 7;
+
+const int SX1_PIN = 10;
+const int SX2_PIN = 25;
 
 Kalman kalman_gyro_X;
 Kalman kalman_gyro_Y;
@@ -46,7 +47,7 @@ Kalman kalman_vel_X;
 Kalman kalman_vel_Y;
 Kalman kalman_vel_Z;
 
-bool SILENT = true;
+bool SILENT = false;
 bool DEBUG = true;
 
 float altitude = 0.0;
@@ -56,13 +57,11 @@ float altitude_calibration = 0.0;
 
 int i = 0;
 
-double kal_pitch;
-double kal_roll;
 double acc_pitch;
 double acc_roll;
 
-int servo_translate(int pos) {
-    return pos + 90;
+int servo_translate(double pos) {
+    return (int) std::clamp(pos, -90.0, 90.0) + 90;
 }
 
 enum RocketState {
@@ -73,22 +72,21 @@ enum RocketState {
     LANDING,
 };
 
-RocketState rocket_state = IDLE;
+RocketState rocket_state = RocketState::IDLE;
+
 unsigned long rocket_start_t = 0;
 unsigned long rocket_boost_t = 0;
 unsigned long rocket_coast_t = 0;
 
-int servo_position_x = 0;
-int servo_position_y = 0;
+double servo_position_x = 0;
+double servo_position_y = 0;
 
-float target_altitude = 850;
+float target_altitude = 820.0 / 3.28084;
 
-double kalPitch;
-double kalRoll;
-double accPitch;
-double accRoll;
+double kal_pitch;
+double kal_roll;
 
-float dt = 0.05;
+float dt = 0.4;
 
 Vector acc_sensor_data;
 Vector gyr_sensor_data;
@@ -96,14 +94,14 @@ Vector gyr_sensor_data;
 Vec3d acc = Vec3d(0, 0, 0);
 Vec3d gyr = Vec3d(0, 0, 0);
 Vec3d vel = Vec3d(0, 0, 0);
-Vec3d last_vel = Vec3d(0, 0, 0);
+Vec3d last_acc = Vec3d(0, 0, 0);
 Vec3d kal_velocity = Vec3d(0, 0, 0);
 
 double adjustment_angle = 0.0;
 
-double m = 500.0 / 1000.0;
+double m = 651.0 / 1000.0;
 
-float k = 0.5 * 1.225 * 0.477 * 34.2 / 0.00342;
+float k = 0.5 * 1.223 * 0.477 * 0.00342;
 
 void setup() {
     Serial.begin(115200);
@@ -155,8 +153,8 @@ void setup() {
 }
 
 // x = -1 to 1, y= -1 to 1 z = 0
-void vector_fins(Vec3d angleVec, double &pitch, double &roll) {
-    servo_position_x = -pitch + angleVec.x;
+void vector_fins(Vec3d angleVec, double pitch, double roll) {
+    servo_position_x = pitch + angleVec.x;
     servo_position_y = roll + angleVec.y;
 
     // Assign the converted integer to all the servos
@@ -177,8 +175,8 @@ void loop() {
     acc_sensor_data = mpu.readNormalizeAccel();
     gyr_sensor_data = mpu.readNormalizeGyro();
 
-    acc = Vec3d((double)acc_sensor_data.XAxis, (double)acc_sensor_data.YAxis, (double)acc_sensor_data.ZAxis);
-    gyr = Vec3d((double)gyr_sensor_data.XAxis, (double)gyr_sensor_data.YAxis, (double)gyr_sensor_data.ZAxis);
+    acc = Vec3d((double) acc_sensor_data.XAxis, (double) acc_sensor_data.YAxis, (double) acc_sensor_data.ZAxis);
+    gyr = Vec3d((double) gyr_sensor_data.XAxis, (double) gyr_sensor_data.YAxis, (double) gyr_sensor_data.ZAxis);
 
     // altitude in meters
     altitude = bmp.readAltitude(SEA_LEVEL_PRESSURE_HPA);
@@ -190,65 +188,90 @@ void loop() {
         calibrate = true;
     }
 
-    // altitude to feet
-    altitude *= 3.28084;
+//    // altitude to feet
+//    altitude *= 3.28084;
 
-    vel.x += 0.5 * (acc.x + last_vel.x) * dt;
-    vel.y += 0.5 * (acc.y + last_vel.y) * dt;
-    vel.z += 0.5 * (acc.z + last_vel.z) * dt;
+    vel.x += 0.5 * (acc.x + last_acc.x) * dt;
+    vel.y += 0.5 * (acc.y + last_acc.y) * dt;
+    vel.z += 0.5 * (acc.z - 9.81 + last_acc.z) * dt;
 
-    kal_velocity = Vec3d(kalman_vel_X.getAngle(vel.x, (acc.x + last_vel.x), dt),
-                         kalman_vel_Y.getAngle(vel.y, (acc.y + last_vel.y), dt),
-                         kalman_vel_Z.getAngle(vel.z, (acc.z + last_vel.z), dt));
+    kal_velocity = Vec3d(kalman_vel_X.getAngle(vel.x, (acc - last_acc).magnitude(), dt),
+                         kalman_vel_Y.getAngle(vel.y, (acc - last_acc).magnitude(), dt),
+                         kalman_vel_Z.getAngle(vel.z, (acc - last_acc).magnitude(), dt));
 
 
-    acc_pitch = -(atan2(acc.x, sqrt(acc.y * acc.y + acc.z * acc.z)) * 180.0) / M_PI;
-    acc_roll = (atan2(acc.y, acc.y) * 180.0) / M_PI - 90;
+    acc_pitch = -degrees(atan2(acc.x, sqrt(acc.y * acc.y + acc.z * acc.z)));
+    acc_roll = -degrees(atan2(acc.y, acc.z)) + 90;
+
 
 
     //dt is the time between readings in seconds
     //TODO: Adust as necessary
-    kalPitch = kalman_acc_X.getAngle(accPitch, gyr.y, dt);
-    kalRoll = kalman_acc_Y.getAngle(accRoll, gyr.y, dt);
+    kal_pitch = kalman_acc_X.getAngle(acc_pitch, gyr.y, dt);
+    kal_roll = kalman_acc_Y.getAngle(acc_roll, gyr.y, dt);
 
     // Assuming h_target, h_current, k, v (velocity), m (mass), and g (gravitational constant) are already defined
 
-    adjustment_angle = acos((target_altitude - altitude) /
-                            (m * log(1 + pow(k * vel.z / (m * 9.80665), 2))));
+    vel.z = vel.z > 1.0 ? vel.z : 3.0;
+
+    adjustment_angle = degrees(
+            acos((target_altitude - altitude) * (2.0 * k / (m * log(1 + k * vel.z * vel.z / (m * 9.80665))))));
+
+    if (std::isnan(adjustment_angle))
+        adjustment_angle = 0.0;
 
 
     switch (rocket_state) {
         case IDLE:
-            if ((millis() - rocket_start_t > 5000) && (abs(acc.z) > 25.0)) { //TODO check acc
+            if ((millis() - rocket_start_t > 5000) && (abs(acc.z) > 15.0)) { //TODO check acc
                 rocket_state = BOOST;
                 rocket_boost_t = millis();
             }
             break;
         case BOOST:
-            if (millis() - rocket_boost_t > 500 && acc.z < 0) {
+
+            sx1.write(servo_translate(0));
+            sx2.write(servo_translate(0));
+            sy1.write(servo_translate(0));
+            sy2.write(servo_translate(0));
+
+            if (millis() - rocket_boost_t > 1000 && acc.z < 10) {
                 rocket_state = COAST;
                 rocket_coast_t = millis();
             }
             break;
         case COAST:
-            vector_fins(Vec3d(adjustment_angle, 0, 0), kalPitch, kalRoll);
+//            vector_fins(Vec3d(adjustment_angle, 0, 0), acc_pitch, acc_roll);
 
-            if (millis() - rocket_coast_t > 2000 && altitude > target_altitude) {
+            servo_position_x = kal_pitch + adjustment_angle;
+            servo_position_y = kal_roll + 0;
+
+            // Assign the converted integer to all the servos
+            sx1.write(servo_translate(servo_position_x));
+            sx2.write(servo_translate(-servo_position_x));
+            sy1.write(servo_translate(servo_position_y));
+            sy2.write(servo_translate(-servo_position_y));
+
+//            char buffer[100];  // Buffer to hold the formatted string
+//            sprintf(buffer, "%4.2f %4.2f %4.2f %4.2f %4.2f %4.2f %4.2f ", target_altitude, altitude, m, k, vel.z,
+//                    (target_altitude - altitude) * ((2.0 * k) / (m * (log(1 + (k * vel.z * vel.z) / (m * 9.80665))))),
+//                    adjustment_angle);
+//            Serial.println(buffer);
+
+            if (millis() - rocket_coast_t > 2000 && altitude > target_altitude-15) {
                 rocket_state = APOGEE;
             }
             break;
         case APOGEE:
-            if (altitude < 300) {
+            if (altitude < target_altitude-30) {
                 rocket_state = LANDING;
             }
             break;
         case LANDING:
-            if (altitude < 0) {
-                sx1.write(90);
-                sx2.write(90);
-                sy1.write(90);
-                sy2.write(90);
-            }
+            sx1.write(servo_translate(90));
+            sx2.write(servo_translate(90));
+            sy1.write(servo_translate(90));
+            sy2.write(servo_translate(90));
             if (!SILENT) {
                 digitalWrite(BUZZER_PIN, HIGH); // turn the buzzer on
                 delay(100); // wait for 200ms
@@ -260,12 +283,16 @@ void loop() {
             }
             break;
     }
-    last_vel = acc;
+
+    if (millis() - rocket_start_t > 100000){
+        digitalWrite(BUZZER_PIN, HIGH); // turn the buzzer on
+    }
+    last_acc = acc;
 
     if (DEBUG) {
-        char buffer[100];  // Buffer to hold the formatted string
-        sprintf(buffer, "%4.2f %4.2f %4.2f %4.2f", acc.x, acc.y, acc.z, kal_pitch);
-        Serial.println(buffer);
+//        char buffer[100];  // Buffer to hold the formatted string
+//        sprintf(buffer, "%4.2f %4.2f %4.2f %4.2f", acc.x, acc.y, acc.z, kal_pitch);
+//        Serial.println(buffer);
     }
     delay(50);
 }
